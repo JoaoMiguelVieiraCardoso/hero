30e22f3cbdb444a40113b72ba3db84026062d4ce
jomi <up202108732@up.pt>
2022-10-03 01:31:13 +0100

    gamesssssess

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 public class Application {
     public static void main(String[] args) {
         Game game = new Game();
+        Hero hero = new Hero(10, 10);
         try {
             game.run();
         }catch (IOException e){
---
+++ src/main/java/Hero.java
@@ -0,0 +1,14 @@
+public class Hero {
+    private int x;
+    private int y;
+    public Hero(int x_, int y_){
+        x = x_;
+        y = y_;
+    }
+    public int get_x(){
+        return x;
+    }
+    public int get_y(){
+        return y;
+    }
+}
